e419b0603fe870e97131212f3174ddf9379f325d
Brandon Diep <brandondiep2711@gmail.com>
2025-04-06 22:54:50 -0700

    added new navbar and made old navbar a separate component

--- components/navbar1.tsx
+++ components/navbar1.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Menu, X } from 'lucide-react'
 import Link from "next/link"
 import { useTheme } from "next-themes"
 import { motion, AnimatePresence } from "framer-motion"
 
-export default function Navbar() {
+export default function Navbar1() {
+
+  const [isScrolled, setIsScrolled] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
   const { theme } = useTheme()
-
 
   const navItems = [
     { name: 'Home', href: '/' },
@@ -17,10 +18,28 @@ export default function Navbar() {
     { name: 'Login', href: '/login'}
   ]
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 50){
+        setIsScrolled(true)
+      }
+      else{
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
 
   return (
     <motion.header
-      className={`sticky top-0 z-50 w-full border-b ${isOpen ? "bg-white" : "bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"}`}      
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-white shadow-md" : "bg-transparent"}`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.6 }}
@@ -61,7 +80,7 @@ export default function Navbar() {
               animate={{ opacity: 1, height: 'auto' }}
               exit={{ opacity: 0, height: 0 }}
               transition={{ duration: 0.3 }}
-              className={`md:hidden absolute top-full left-0 right-0 ${theme === "light" ? "bg-white" : "bg-white"} shadow-md overflow-hidden py-4`}
+              className={`md:hidden absolute top-full left-0 right-0 ${theme === "light" ? "bg-white" : "bg-white-950"} shadow-md overflow-hidden py-4`}
             >
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col space-y-4">
                 {navItems.map((nav) => (
