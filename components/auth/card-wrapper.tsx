c391c56d91bdc30d5a2a8e98f29c1bd8b12b8b8c
Brandon Diep <brandondiep2711@gmail.com>
2025-04-08 12:28:43 -0700

    worked on login page and added schema for login

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { Card, CardHeader, CardContent, CardFooter } from "../ui/card"
+import Social from "./social"
+import { Button } from "../ui/button"
+import Link from "next/link"
+
+interface CardWrapperProps {
+    children: React.ReactNode
+    headerLabel: string
+    backButtonLabel: string
+    backButtonHref: string
+    showSocial?: boolean
+}
+
+export default function CardWrapper({ children, headerLabel, backButtonLabel, backButtonHref, showSocial }: CardWrapperProps) {
+  return (
+    <Card className="w-[400px] shadow-md">
+        <CardHeader className='w-full flex flex-col gap-y-4 items-center justify-center'>
+            <h1 className='text-3xl font-semibold'>Welcome back</h1>
+            <p className='text-muted-foreground text-sm'>{headerLabel}</p>
+        </CardHeader>
+        
+        <CardContent>
+            {children}
+        </CardContent>
+        
+        {showSocial && (
+            <CardFooter>
+                <Social />
+            </CardFooter>
+        )}
+        <CardFooter>
+            <Button variant="link" className="font-normal w-full" size="sm">
+                <Link href={backButtonHref}>{backButtonLabel}</Link>
+            </Button>
+        </CardFooter>
+    </Card>
+  )
+}
