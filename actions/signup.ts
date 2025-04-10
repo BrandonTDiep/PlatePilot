e9f2b705d9b1d83ceed3402afc14d656537b449e
Brandon Diep <brandondiep2711@gmail.com>
2025-04-10 00:46:18 -0700

    added middleware and more auth files

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,9 +1,10 @@
 "use server"
 
 import * as z from "zod"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { SignUpSchema } from "@/schemas"
 import { db } from "@/lib/db"
+import { getUserByEmail } from "@/services/user"
 
 export const signup = async(values: z.infer<typeof SignUpSchema>) => {
   const validatedFields = SignUpSchema.safeParse(values)
@@ -16,11 +17,7 @@ export const signup = async(values: z.infer<typeof SignUpSchema>) => {
 
   const hashedPassword = await bcrypt.hash(password, 10)
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    }
-  })
+  const existingUser = await getUserByEmail(email)
 
   if (existingUser) {
     return { error: "Email already in use!" }
